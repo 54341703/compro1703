dc9be444d6398172110d281f20b02a9ffc8813d2
Pongkamon <bomos_2678@hotmail.com>
2012-06-19 14:22:52 +0700

    เพิ่มProgramming is fun

--- src/th/ac/nu/student/u54341703/compro/ch01/HelloWorld.java
+++ src/th/ac/nu/student/u54341703/compro/ch01/HelloWorld.java
@@ -1,9 +1,9 @@
-
 package th.ac.nu.student.u54341703.compro.ch01;
 
 
 public class HelloWorld {
     public static void main(String[]args){
         System.out.println("Hello,World");
+        System.out.println("Programming is fun");
     }
 }
