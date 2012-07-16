144e0cb22d0b4d347b3dfe4cf6a80cc43467c2e3
Pongkamon  Chomnuch <bomos_2678@hotmail.com>
2012-07-16 19:17:31 +0700

    Chapter 03

---
+++ src/th/ac/nu/student/u54341703/compro/ch03/DanglingElse.java
@@ -0,0 +1,14 @@
+package th.ac.nu.student.u54341703.compro.ch03;
+public class DanglingElse {
+
+ public static void main(String[] args) {
+ int x = 5;
+ if (x < 5) {
+ if (x > 5) {
+ System.out.println('b');
+ } else {
+ System.out.println('a');
+ }
+ }
+ }
+}
